60a468029922f671cf85ae1c42f01b64cbc49c1f
James <james@jemz.org>
2017-01-17 14:51:02 +0700

    adding mongoose

---
+++ app/models/clicks.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Click = new Schema(
+	{clicks: Number},
+	{versionKey: false}
+	);
+
+module.exports = mongoose.model('Click', Click);
--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongo = require('mongodb').MongoClient;
+	
 
+	var express = require('express');
+	var routes = require('./app/routes/index.js');
+	var mongoose = require('mongoose');
 
-var app = express();
-var port = process.env.PORT || 8080;
-var url = 'mongodb://sample:sample@ds157248.mlab.com:57248/paidb'
+	var app = express();
+	var port = process.env.PORT || 8080;
+	var url = 'mongodb://sample:sample@ds157248.mlab.com:57248/paidb'
 
-mongo.connect(url, function(err, db){
-
-	if (err) {
-		throw new Error('failed to connect to database');
-	} else {
-		console.log('MongoDB successfully connected to: ' + url);
-	}
+	mongoose.connect(url);
 
 
 	app.use('/public', express.static(process.cwd() + '/public'));
 	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-	routes(app, db);
+	routes(app);
 
 	app.listen(port, function(){
 		console.log('Example app listening on port ' + port);
 	});
 
-
-});
